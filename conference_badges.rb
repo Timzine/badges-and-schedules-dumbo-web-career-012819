2d25422defd5fdf5f4e4fd22725493f53535af12
Lemrabt Boudadya <lboudadya@gmail.com>
2018-12-29 19:51:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,6 +6,6 @@ def badge_maker(name)
 end 
 
 def batch_badge_creator(array)
-     array.push("Hello, my name is #{name}.")
-     return array
+     new_array = array.push("Hello, my name is #{name}.")
+     return new_array
 end  
