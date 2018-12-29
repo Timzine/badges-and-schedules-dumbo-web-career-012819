a8c489ab02e4f0ce9940df90250c82ffd5b89997
Lemrabt Boudadya <lboudadya@gmail.com>
2018-12-29 20:25:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,6 +9,7 @@ def batch_badge_creator(people_array)
     #people_array.map do |person|
     #  "Hello, my name is #{person}."
     #end
+    
     new_array = []
     people_array.each do |person|
      new_array.push( "Hello, my name is #{person}.")
